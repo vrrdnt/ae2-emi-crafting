ea8bd4ac16430a96839a6fd754f5329f7fd94c25
alikindsys <alice@blocovermelho.org>
2026-06-04 22:44:20 -0300

    Add a mod logger instance

--- src/main/java/org/blocovermelho/ae2emi/platform/facade/Ae2EmiMod.java
+++ src/main/java/org/blocovermelho/ae2emi/platform/facade/Ae2EmiMod.java
@@ -1,6 +1,10 @@
 package org.blocovermelho.ae2emi.platform.facade;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Ae2EmiMod {
+    public static Logger LOGGER = LoggerFactory.getLogger(Ae2EmiMod.class);
     public static void onInitialize() {
         // Cross-platform initialization code.
     }
