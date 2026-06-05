c539396eb842ddce11a0a764da102b9676134657
alikindsys <alice@blocovermelho.org>
2026-06-04 22:45:30 -0300

    Add support for charger recipes

--- src/main/java/org/blocovermelho/ae2emi/platform/facade/Ae2EmiPlugin.java
+++ src/main/java/org/blocovermelho/ae2emi/platform/facade/Ae2EmiPlugin.java
@@ -1,7 +1,11 @@
 package org.blocovermelho.ae2emi.platform.facade;
 
+import appeng.core.definitions.AEBlocks;
+import appeng.recipes.handlers.ChargerRecipe;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.stack.EmiStack;
+import org.blocovermelho.ae2emi.platform.facade.crafting.recipe.Ae2ChargerRecipe;
 import org.blocovermelho.ae2emi.platform.facade.crafting.recipe.RecipeUtil;
 import org.blocovermelho.ae2emi.platform.facade.crafting.recipe.category.Ae2Categories;
 
@@ -10,5 +14,9 @@ public class Ae2EmiPlugin implements EmiPlugin {
     public void register(EmiRegistry registry) {
         registry.addCategory(Ae2Categories.ATTUNEMENT);
         RecipeUtil.registerAttunement(registry);
+
+        registry.addCategory(Ae2Categories.CHARGER);
+        RecipeUtil.registerAllByType(registry, ChargerRecipe.TYPE, Ae2ChargerRecipe::new);
+        registry.addWorkstation(Ae2Categories.CHARGER, EmiStack.of(AEBlocks.CHARGER));
     }
 }
