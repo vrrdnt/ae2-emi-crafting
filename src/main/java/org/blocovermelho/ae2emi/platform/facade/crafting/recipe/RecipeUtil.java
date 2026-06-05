120a42cc3ff46584494b04e59b44819d154768d1
alikindsys <alice@blocovermelho.org>
2026-06-04 22:44:58 -0300

    Add helper for registering all recipes from the same type

--- src/main/java/org/blocovermelho/ae2emi/platform/facade/crafting/recipe/RecipeUtil.java
+++ src/main/java/org/blocovermelho/ae2emi/platform/facade/crafting/recipe/RecipeUtil.java
@@ -4,10 +4,26 @@ import appeng.api.features.P2PTunnelAttunementInternal;
 import appeng.core.localization.ItemModText;
 import dev.emi.emi.api.EmiApi;
 import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiRecipe;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeType;
+import org.blocovermelho.ae2emi.platform.facade.Ae2EmiMod;
+
+import java.util.List;
+import java.util.function.Function;
 
 public class RecipeUtil {
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static <T extends Recipe<?>> void registerAllByType(EmiRegistry registry, RecipeType type, Function<T, EmiRecipe> mapper) {
+        List<T> recipes = (List<T>) registry.getRecipeManager().getAllRecipesFor(type);
+        for (T recipe : recipes) {
+            registry.addRecipe(mapper.apply(recipe));
+        }
+        Ae2EmiMod.LOGGER.info("Registered {} recipes for {}", recipes.size(), type.getClass().getName());
+    }
+
     public static void registerAttunement(EmiRegistry registry) {
         for (var entry : P2PTunnelAttunementInternal.getApiTunnels()) {
             var inputs = EmiApi.getIndexStacks().stream().filter(x -> entry.stackPredicate().test(x.getItemStack())).toList();
